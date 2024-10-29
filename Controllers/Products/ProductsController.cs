f4ef67850f1691467036195023fdb7c3b61d25a0
rj <rj@rjfrdvlpr.rjfrdvlpr>
2024-10-29 03:46:20 -0500

    RDY

---
+++ Controllers/Products/ProductsController.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+using TEST_CRUD.Datos;
+using TEST_CRUD.Models;
+
+namespace TEST_CRUD.Controllers
+{
+    public class ProductsController : Controller
+    {
+        empleadosDatos _empleadosDatos = new empleadosDatos();
+        public IActionResult Listar_Products()
+        {
+            return View();
+        }
+        public IActionResult FGuardar_Personal()
+        {
+			var oRoles = _empleadosDatos.ListarR();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FGuardar_Personal(empleadosModel oGuardarP)
+        {
+            var save = _empleadosDatos.Guardar(oGuardarP);
+            if (save)
+                return RedirectToAction("Listar_Personal");
+            else
+                return View();
+        }
+        public IActionResult FEditar_Personal(int I_ID)
+        {
+            var oID_Roles = _empleadosDatos.ObtenerPId(I_ID);
+            return View(oID_Roles);
+        }
+
+        [HttpPost]
+        public IActionResult FEditar_Personal(empleadosModel oI_ID)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var up = _empleadosDatos.Editar(oI_ID);
+
+            if (up)
+                return RedirectToAction("Listar_Personal");
+            else
+                return View();
+        }
+        
+        public IActionResult Eliminar_Personal(int I_ID)
+        {
+            //METODO SOLO DEVUELVE LA VISTA
+            var oDI = _empleadosDatos.ObtenerPId(I_ID);
+            return View(oDI);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar_Personal(empleadosModel odi)
+        {
+
+            var down = _empleadosDatos.Eliminar(odi.empleadoId);
+
+            if (down)
+                return RedirectToAction("Listar_Personal");
+            else
+                return View();
+        }
+    }
+}
+ 
